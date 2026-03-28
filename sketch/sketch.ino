3ad51c5eefddf68f15f76f0a69d93db5aff9a6ca
sai <sai@mail.ru>
2026-03-28 16:55:20 +0700

    add sketch

---
+++ sketch/sketch.ino
@@ -0,0 +1,11 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+void setup() {
+  // put your setup code here, to run once:
+  lcd.init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
